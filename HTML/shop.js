7f052766cfaf3312b7e5bdf7666c65c1ccda5879
MedinaSoftic <medina.m.softic@gmail.com>
2025-05-05 15:02:25 -0500

    added edit button

--- HTML/shop.js
+++ HTML/shop.js
@@ -1,25 +1,36 @@
-const list = document.querySelector('ul');
-const input = document.querySelector('input');
+const list = document.getElementById('itemList');
+const input = document.getElementById('itemInput');
 const button = document.querySelector('button');
-button.onclick = function(){
+button.addEventListener("click" ,function(){
 let newItem = input.value;
 input.value = "";
 
 let itemList = document.createElement('li');
 let itemText = document.createElement('span');
-let itemButton = document.createElement('button');
+let removeButton = document.createElement('button');
+let editButton = document.createElement('button');
 
 itemList.appendChild(itemText);
 itemText.textContent = (newItem);
 
-itemList.appendChild(itemButton);
-itemButton.textContent = 'Remove';
+itemList.appendChild(removeButton);
+removeButton.textContent = 'Remove';
+
+itemList.appendChild(editButton);
+editButton.textContent ='Edit';
 
 list.appendChild(itemList);
 
-itemButton.onclick = function() {
-    list.removeChild(itemList);
-}
+    removeButton.onclick = function() {
+        list.removeChild(itemList);
+    };
 
-input.focus()
-}   
+    editButton.onclick = function() {
+        let updated = prompt("Edit item:", itemText.textContent);
+        if (updated !== null) {
+          itemText.textContent = updated;
+        }
+      };
+
+input.focus();
+});
